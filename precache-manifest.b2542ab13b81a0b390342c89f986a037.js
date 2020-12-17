c2975ea2a0e638d216505caa81841b544a6a811a
jfcere <jfcere@sherweb.com>
2020-12-16 22:02:56 -0500

    Updates

---
+++ precache-manifest.b2542ab13b81a0b390342c89f986a037.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b2bbae6cc3dae577342b88dcfa1b48",
+    "url": "/react-redux-sample/index.html"
+  },
+  {
+    "revision": "3debcf9b1e4023854d77",
+    "url": "/react-redux-sample/static/css/4.ad81aad7.chunk.css"
+  },
+  {
+    "revision": "96d1648fcb31e67f6b14",
+    "url": "/react-redux-sample/static/css/main.8adfbf33.chunk.css"
+  },
+  {
+    "revision": "55c213b1f70667553809",
+    "url": "/react-redux-sample/static/js/2.4a854d2b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/react-redux-sample/static/js/2.4a854d2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9be3238c4ba9368a1fc",
+    "url": "/react-redux-sample/static/js/3.b22c6676.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/react-redux-sample/static/js/3.b22c6676.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3debcf9b1e4023854d77",
+    "url": "/react-redux-sample/static/js/4.7b7a2e81.chunk.js"
+  },
+  {
+    "revision": "d2d390cad0578eb8049c",
+    "url": "/react-redux-sample/static/js/5.1740f61c.chunk.js"
+  },
+  {
+    "revision": "96d1648fcb31e67f6b14",
+    "url": "/react-redux-sample/static/js/main.6a09f169.chunk.js"
+  },
+  {
+    "revision": "4b0a6cd74aab483bc55b",
+    "url": "/react-redux-sample/static/js/runtime-main.ae8705cb.js"
+  },
+  {
+    "revision": "607ba1795c55444e38bed385a9272932",
+    "url": "/react-redux-sample/static/media/captain-america.607ba179.png"
+  },
+  {
+    "revision": "69494fdde8d9cbfa7678188bd0387cb1",
+    "url": "/react-redux-sample/static/media/github.69494fdd.svg"
+  },
+  {
+    "revision": "ceb52d769462555c554cabd448041f38",
+    "url": "/react-redux-sample/static/media/triangle-bg.ceb52d76.png"
+  }
+]);
